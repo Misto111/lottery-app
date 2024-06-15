55b3c122665ea4f2081840c5a4588fbe500cc346
Miroslav Stoev <soboko@abv.bg>
2024-06-15 09:53:24 +0300

    Add files via upload

--- src/Helper/actions.js
+++ src/Helper/actions.js
@@ -2,33 +2,42 @@ import { getRandomNumber, getrandomColor } from "./utils";
 
 export function registerTicket() {
     const newTicket = {
-        number : getRandomNumber(),
-        color : getrandomColor()
+        number: getRandomNumber(),
+        color: getrandomColor()
     };
 
-    this.setState( (prevState) => {
-        prevState.tickets.push( newTicket );
+    this.setState((prevState) => {
+        prevState.tickets.push(newTicket);
 
         return {
-            tickets : prevState.tickets,
-            remainingTickets : -- prevState.remainingTickets
+            tickets: prevState.tickets,
+            remainingTickets: --prevState.remainingTickets
         }
-    } );
+    });
 
 }
 
-export function removeTicket( index ) {
-    this.setState(( prevState ) => {
-          prevState.tickets.splice( index, 1 );
+export function removeTicket(index) {
+    this.setState((prevState) => {
+        prevState.tickets.splice(index, 1);
 
-          return {
-            tickets            : prevState.tickets,
-            remainingTickets   : ++ prevState.remainingTickets
-            
-          }
-    } );
+        return {
+            tickets: prevState.tickets,
+            remainingTickets: ++prevState.remainingTickets
+
+        }
+    });
 }
 
 export function finish() {
-    this.setState( { finished: true } );
+    this.setState({ finished: true });
 }
+
+export function reset() {
+    this.setState( {
+        winningNumber: getRandomNumber(),
+        tickets: [],
+        remainingTickets: 5,
+        finished: false
+    });
+} 
